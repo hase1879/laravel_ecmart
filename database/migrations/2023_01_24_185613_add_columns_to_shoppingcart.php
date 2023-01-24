ade9f869242d75236bc4e51e72191befe7c735e1
hase1879 <kamhuta1438@gmail.com>
2023-01-24 19:00:46 +0900

    a

--- database/migrations/2023_01_24_185613_add_columns_to_shoppingcart.php
+++ database/migrations/2023_01_24_185613_add_columns_to_shoppingcart.php
@@ -14,9 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('shoppingcart', function (Blueprint $table) {
-            $table->string('code')->default("");
-            $table->integer('price_total')->unsigned()->default(0);
-            $table->integer('qty')->unsigned()->default(0);
+            //
         });
     }
 
@@ -29,7 +27,6 @@ return new class extends Migration
     {
         Schema::table('shoppingcart', function (Blueprint $table) {
             //
-
         });
     }
 };
